5b679fbb1562562a6db49902f805830badd7188b
Belle Tissott <belletissott@yahoo.com>
2019-04-16 10:08:07 +1000

    Remove unused code, tidy line and word spacing

--- load_landsat_nbart_ts.py
+++ load_landsat_nbart_ts.py
@@ -1,25 +1,18 @@
-
 # coding: utf-8
+# Load sentinel time series data, save the data as ENVI images
 
-# # Load sentinel time series data, save the data as ENVI images
-
-# In[40]:
-
-
-# Import modules
 import datacube
 import sys
 import numpy as np
 import time
 import os
-    
-
-
-# In[41]:
-
-
 import rasterio
-def write_multi_time_dataarray(timebandnaes, filename, dataarray, **profile_override):
+
+# Import dea-notebooks functions using relative link to Scripts directory
+sys.path.append('/g/data1/u46/pjt554/dea_notebooks/dea-notebooks-master/10_Scripts')
+import DEADataHandling
+
+def write_multi_time_dataarray(filename, dataarray, **profile_override):
     profile = {
         'width': len(dataarray[dataarray.crs.dimensions[1]]),
         'height': len(dataarray[dataarray.crs.dimensions[0]]),
@@ -33,25 +26,10 @@ def write_multi_time_dataarray(timebandnaes, filename, dataarray, **profile_over
     with rasterio.open(str(filename), 'w', **profile) as dest:
         for time_idx in range(len(dataarray.time)):
             bandnum = time_idx + 1
-            bandstr=timebandnames[time_idx]
             dest.write(dataarray.isel(time=time_idx).data, bandnum)
-            
 
 
-
-
-
-# In[42]:
-
-
-# Import dea-notebooks functions using relative link to Scripts directory
-
-sys.path.append('/g/data1/u46/pjt554/dea_notebooks/dea-notebooks-master/10_Scripts')
-import DEADataHandling
-    
-    
-#bands_of_interest=['nbart_red', 'nbart_green', 'nbart_blue', 'nbart_nir_2', 'nbart_swir_2', 'nbar_swir_3']
-# Connect to a datacube containing Sentinel data
+# Connect to a datacube
 dc = datacube.Datacube(app='Clear Landsat')
 
 argc = len(sys.argv)
@@ -62,7 +40,7 @@ if (argc != 8):
     sys.exit() 
 
 # Set up spatial and temporal query; note that 'output_crs' and 'resolution' need to be set
-param=sys.argv
+param = sys.argv
 
 lat_top = float(param[1])
 lat_bottom = float(param[2])
@@ -75,30 +53,14 @@ dirc = param[7]
 comm = 'mkdir ' + dirc
 os.system(comm)
 
-
-#lon_left=149.02
-#lon_right=149.22
-#lat_top=-35.11
-#lat_bottom=-35.35
-#epoh_beg='2015-01-01'
-#epoh_end='2015-12-31'
-
-newquery={'x': (lon_left, lon_right),
-          'y': (lat_top, lat_bottom),
-          'time': (start_of_epoch, end_of_epoch)
-         }
-
-
-query = {'x': (149.02, 149.22),
-         'y': (-35.11, -35.35),
-         'time': ('2015-01-01', '2015-12-31')
-        }   
-
+query={'x': (lon_left, lon_right),
+       'y': (lat_top, lat_bottom),
+       'time': (start_of_epoch, end_of_epoch)
+       }
 
 allbands=['blue', 'green', 'red', 'nir', 'swir1', 'swir2']
 output_bandnames = ['blue','green', 'red', 'nir', 'swir1', 'swir2']
 
-#dirc = '/g/data1/u46/pjt554/urban_change_nbart_full_sites/act_canberra'
 cc=0
 # Load observations with less than 70% cloud from both S2A and S2B as a single combined dataset
 for bandname in allbands:
@@ -106,24 +68,20 @@ for bandname in allbands:
     inbandlist=[]
     inbandlist.append(bandname)
     print(inbandlist)
-    landsat_ds = DEADataHandling.load_clearlandsat(dc=dc, query=newquery, sensors=('ls5', 'ls7', 'ls8'), product='nbart',
+    landsat_ds = DEADataHandling.load_clearlandsat(dc=dc, query=query, sensors=('ls5', 'ls7', 'ls8'), product='nbart',
                                        bands_of_interest=inbandlist, ls7_slc_off=True,
                                        mask_pixel_quality=False, mask_invalid_data=False, masked_prop=0.0)
     
     timelist = []
     for t in landsat_ds['time']:
         timelist.append(time.strftime("%Y-%m-%d", time.gmtime(t.astype(int)/1000000000)))
-    
-    timebandnames=np.asarray(timelist)
 
-    banddata=landsat_ds[bandname]
-    outbandname=output_bandnames[cc]
+    timebandnames = np.asarray(timelist)
+    banddata = landsat_ds[bandname]
+    outbandname = output_bandnames[cc]
     filename=dirc+'/NBAR_'+outbandname+'.img'
-    write_multi_time_dataarray(timebandnames, filename, banddata, driver='ENVI')
+    write_multi_time_dataarray(filename, banddata, driver='ENVI')
     cc=cc+1
-    #landsat_ds = None
-
-# In[43]:
 
 
 def create_envi_header(fname, icol, irow, ts, lon_left, lat_top, description, bandnames):
@@ -148,39 +106,28 @@ def create_envi_header(fname, icol, irow, ts, lon_left, lat_top, description, ba
     hdr_file.write(outstr)
     hdr_file.write("wavelength units =\n")
     hdr_file.write("band names ={\n")
-    cc=0
+
+    cc = 0
     for bandstr in bandnames:
-        if (cc!=ts-1):
-            outstr=bandstr+',\n'
+        if (cc != ts - 1):
+            outstr = bandstr + ',\n'
         else:
-            outstr=bandstr+'\n'
+            outstr = bandstr + '\n'
+
         hdr_file.write(outstr)
-        cc=cc+1
+        cc = cc + 1
+
     hdr_file.write('}\n')    
     hdr_file.close()
 
+fname = dirc + '/clouds.hdr'
 
-
-# In[ ]:
-
-
-
-
-
-
-# In[44]:
-
-
-
-
-fname=dirc+'/clouds.hdr'
-
-xs= landsat_ds['x'].size
-ys= landsat_ds['y'].size
-ts= landsat_ds['time'].size
-des='cloud = 3, non_cloud = 0'
-lon_left=1.0
-lat_top=1.0
+xs = landsat_ds['x'].size
+ys = landsat_ds['y'].size
+ts = landsat_ds['time'].size
+des = 'cloud = 3, non_cloud = 0'
+lon_left = 1.0
+lat_top = 1.0
 create_envi_header(fname, xs, ys, ts, lon_left, lat_top, des, timebandnames)
 
 
