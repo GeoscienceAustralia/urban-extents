aa00e5233ea034c8cb63c734068cb2a7221c92f6
sean.chua@ga.gov.au <sean.chua@ga.gov.au>
2020-03-03 13:18:39 +1100

    add todo's and new location

--- indices_cluster_raw.py
+++ indices_cluster_raw.py
@@ -1,5 +1,5 @@
-
-# This program reads 3 Landsat surface reflectance-derived data, applies k-mean clustering algorithm to generate M clusters, which will be combined to form
+# This program reads 3 Landsat surface reflectance-derived data, applies k-mean clustering algorithm to generate M
+# clusters, which will be combined to form
 # urban and non-urban classes in the next step in the work flow
 
 
@@ -13,204 +13,176 @@ from sklearn.cluster import KMeans
 from spectral import *
 import urban_module as ubm
 
-
 # directory where the input indices data can be found
-path=sys.argv[1]
+path = sys.argv[1]
 
 # file name of the brightness index for the clusters image 
-brightnessfile=sys.argv[2]
+brightnessfile = sys.argv[2]
 
 # file name of the greenness index for the clusters image 
-greennessfile=sys.argv[3]
+greennessfile = sys.argv[3]
 
 # file name of the wetness index for the clusters image 
-wetnessfile=sys.argv[4]
+wetnessfile = sys.argv[4]
+
+#TODO: Add path to smad here
 
 # the number of clusters to generate 
-numcls=int(sys.argv[5])
+numcls = int(sys.argv[5])
 
+filelist = [brightnessfile, greennessfile, wetnessfile]
 
-filelist=[brightnessfile, greennessfile, wetnessfile]
+hdrfile = path + '/' + brightnessfile + '.hdr'
+h = envi.read_envi_header(hdrfile)
 
+nrow = np.int32(h['lines'])
+ncol = np.int32(h['samples'])
+pnum = nrow * ncol
 
-hdrfile=path+'/'+brightnessfile+'.hdr'
-h=envi.read_envi_header(hdrfile)
-
-nrow=np.int32(h['lines'])
-ncol=np.int32(h['samples'])
-pnum = nrow*ncol
-
-data=np.zeros([pnum, 3], dtype=np.float32)
-
-
+#TODO: Change number of dimensions to 4
+data = np.zeros([pnum, 3], dtype=np.float32)
 
 # a function to read indices data file, extract the relevant band in the file
 # put the data in the columns of an array as inputs for the clustering algorithm 
 
-col=0
+col = 0
 for filename in filelist:
     h, oneimage, pnum = ubm.load_envi_data_float(path, filename)
-    data[:, col]= oneimage
-    col=col+1
+    data[:, col] = oneimage
+    col = col + 1
 
-
-
-
+#TODO: modify to another covariate
 def classifyoneblock_spectral(data, nrow, ncol, rp, numcls):
-    
-    pnum=nrow*ncol
-    
+    """
+    This function calls the clustering function and masks water pixels
+    :param data: input array
+    :param nrow: dimensions
+    :param ncol: dimensions
+    :param rp:
+    :param numcls: Number of classes
+    :return:
+    """
+    pnum = nrow * ncol
+
     greenness = data[:, 1]
     brightness = data[:, 0]
     wetness = data[:, 2]
 
-    waterthd=0.08
-   
+    waterthd = 0.08
 
-    #exclude water pixels from input data
+    # exclude water pixels from input data
 
-    wateridx=np.where(wetness>=waterthd)[0]
-    nonwateridx=np.where(wetness<waterthd)[0]
+    wateridx = np.where(wetness >= waterthd)[0]
+    nonwateridx = np.where(wetness < waterthd)[0]
     stidx = np.argsort(greenness)
-    
-    
-    #load non-water pixels data as input for clustering algorithm
-    sprst=data
-    sprst=sprst[nonwateridx]
-    sprst=np.asarray(sprst)
 
+    # load non-water pixels data as input for clustering algorithm
+    sprst = data
+    sprst = sprst[nonwateridx]
+    sprst = np.asarray(sprst)
 
-    # re-scale the data 
-    sprst_scale=preprocessing.scale(sprst)
-    
-    width=2
-    
+    # re-scale the data
+    sprst_scale = preprocessing.scale(sprst)
 
-    clscount=np.zeros([pnum, numcls], dtype=np.int32)
+    width = 2
+
+    clscount = np.zeros([pnum, numcls], dtype=np.int32)
 
     for i in range(rp):
         clsimg = clusterclassifier(sprst_scale, pnum, numcls, wateridx, nonwateridx, brightness, greenness, wetness)
         for k in range(pnum):
-            m=clsimg[k]
-            clscount[k, m]=clscount[k,m]+1
+            m = clsimg[k]
+            clscount[k, m] = clscount[k, m] + 1
 
+    bbclsimg = clscount.argmax(axis=1)
+    bbclsimg = bbclsimg.astype(np.int8)
 
-
-
-    bbclsimg=clscount.argmax(axis=1)
-    bbclsimg=bbclsimg.astype(np.int8)
-
- 
     return bbclsimg
 
-
-
-
-
-
+# TODO: modify to add another covariate
 def clusterclassifier(scaled_data, pnum, numcls, wateridx, nonwateridx, brightness, greenness, wetness):
-    
     clsimg = np.zeros(pnum, dtype=np.int8)
-    clsimg[wateridx]=-1
-    clustering=KMeans(init='k-means++', precompute_distances=True, algorithm="full", n_clusters=numcls-1)
-    clustering.n_jobs=6
-    labels=clustering.fit(scaled_data)
-    clsimg[nonwateridx]=labels.labels_
-    clsimg[:]=clsimg[:]+1
-   
-    cc =np.zeros(numcls)
-    greencc=np.zeros(numcls)
-    brightcc=np.zeros(numcls)
-    wetcc=np.zeros(numcls)
+    clsimg[wateridx] = -1
+    clustering = KMeans(init='k-means++', precompute_distances=True, algorithm="full", n_clusters=numcls - 1)
+    clustering.n_jobs = 6
+    labels = clustering.fit(scaled_data)
+    clsimg[nonwateridx] = labels.labels_
+    clsimg[:] = clsimg[:] + 1
+
+    cc = np.zeros(numcls)
+    greencc = np.zeros(numcls)
+    brightcc = np.zeros(numcls)
+    wetcc = np.zeros(numcls)
 
     for i in range(pnum):
-        clab=clsimg[i]
-        greencc[clab]=greencc[clab]+greenness[i]
-        brightcc[clab]=brightcc[clab]+brightness[i]
-        wetcc[clab]=wetcc[clab]+wetness[i]
-        cc[clab]=cc[clab]+1
-        
+        clab = clsimg[i]
+        greencc[clab] = greencc[clab] + greenness[i]
+        brightcc[clab] = brightcc[clab] + brightness[i]
+        wetcc[clab] = wetcc[clab] + wetness[i]
+        cc[clab] = cc[clab] + 1
+
     for i in range(numcls):
-        if (cc[i]>0):
-            greencc[i]=greencc[i]/cc[i]
-            brightcc[i]=brightcc[i]/cc[i]
-            wetcc[i]=wetcc[i]/cc[i]
-    
-    
-    greencc[0]=1000
+        if (cc[i] > 0):
+            greencc[i] = greencc[i] / cc[i]
+            brightcc[i] = brightcc[i] / cc[i]
+            wetcc[i] = wetcc[i] / cc[i]
+
+    greencc[0] = 1000
     stidx = np.argsort(greencc)
 
-
-    maps=np.zeros(numcls, dtype=np.int32)
+    maps = np.zeros(numcls, dtype=np.int32)
     for i in range(numcls):
-        k=numcls-1-i
-        maps[stidx[k]]=i
-    
+        k = numcls - 1 - i
+        maps[stidx[k]] = i
+
     for i in range(pnum):
-        clsimg[i]=maps[clsimg[i]]
-    
+        clsimg[i] = maps[clsimg[i]]
+
     return clsimg
 
 
+rp = 5
 
+clsimg = classifyoneblock_spectral(data, nrow, ncol, rp, numcls)
+clsimg = clsimg.astype(np.int8)
 
+sprst = preprocessing.scale(data[:, 0:3])
 
+cores = np.zeros([numcls, 3])
+dist = np.zeros([numcls, 2])
 
-
-
-rp=5
-
-clsimg=classifyoneblock_spectral(data, nrow, ncol, rp, numcls)
-clsimg=clsimg.astype(np.int8)
-
-
-
-sprst=preprocessing.scale(data[:, 0:3])
-
-cores=np.zeros([numcls,3])
-dist=np.zeros([numcls,2])
-
-  
 for i in range(numcls):
-    classidx=np.where(clsimg==i)[0]
-    apm=sprst[classidx]
-    cores[i, :]=apm.mean(axis=0)
- 
-  
-for i in range(1, numcls):
-    dist[i, 0]=np.sqrt(((cores[i, :]-cores[1,:])**2).sum(0))
-    dist[i, 1]=np.sqrt(((cores[i, :]-cores[numcls-1,:])**2).sum(0))
-  
-  
-mapcls=np.zeros(numcls)
-mapcls=mapcls.astype(np.int8)
-
-midpp=int(numcls/2)+1
+    classidx = np.where(clsimg == i)[0]
+    apm = sprst[classidx]
+    cores[i, :] = apm.mean(axis=0)
 
 for i in range(1, numcls):
-    if (i<midpp):
-        mapcls[i]=1
+    dist[i, 0] = np.sqrt(((cores[i, :] - cores[1, :]) ** 2).sum(0))
+    dist[i, 1] = np.sqrt(((cores[i, :] - cores[numcls - 1, :]) ** 2).sum(0))
+
+mapcls = np.zeros(numcls)
+mapcls = mapcls.astype(np.int8)
+
+midpp = int(numcls / 2) + 1
+
+for i in range(1, numcls):
+    if (i < midpp):
+        mapcls[i] = 1
     else:
-        mapcls[i]=3
-      
-          
-fstem='urban_spec_5c_raw'
-bandnames=['land cover class raw']
-bands=1
-datatype=1
-description='Land cover classification raw'
+        mapcls[i] = 3
+
+fstem = 'urban_spec_5c_raw'
+bandnames = ['land cover class raw']
+bands = 1
+datatype = 1
+description = 'Land cover classification raw'
 ubm.outputenvifile(clsimg, path, h, fstem, bandnames, bands, datatype, description)
 
-
-bbclsimg=mapcls[clsimg]
+bbclsimg = mapcls[clsimg]
 
 fstem = 'urban_spec_5c'
-bandnames=['land cover class']
-bands=1
-datatype=1
-description='Land cover classification'
+bandnames = ['land cover class']
+bands = 1
+datatype = 1
+description = 'Land cover classification'
 ubm.outputenvifile(bbclsimg, path, h, fstem, bandnames, bands, datatype, description)
-
-
-
-
--- run_indices.sh
+++ run_indices.sh
@@ -1,27 +1,44 @@
 #!/bin/bash
 
 # Output directory
-dir='/g/data/u46/users/bt2744/work/data/urban_extent/tennant_ck'
+dir='/g/data/u46/users/sc0554/LCCS/urban/alice_springs'
 
 
 # Change these to select a different area to train on
 ########################
 
 # Alice Springs
-#lat_top=-23.66
-#lat_bottom=-23.86
-#lon_left=149.75
-#lon_right=149.95
+lat_top=-23.66
+lat_bottom=-23.86
+lon_left=133.70
+lon_right=133.9
+sensor='ls8'
+year='2018'
+
+# Tennant Creek
+#lat_top=-19.5
+#lat_bottom=-19.8
+#lon_left=134
+#lon_right=134.3
 #sensor='ls8'
 #year='2016'
 
-# Tennant Creek
-lat_top=-19.5
-lat_bottom=-19.8
-lon_left=134
-lon_right=134.3
-sensor='ls8'
-year='2016'
+# Canberra
+# lat_top=-35.11
+# lat_bottom=-35.35
+# lon_left=149.02
+# lon_right=149.22
+# sensor='ls8'
+# year='2017'
+# dirc='/g/data1a/u46/pjt554/urban_geomedian_data/canberra'
+
+#test spot
+#lat_top=-29
+#lat_bottom=-29.5
+#lon_left=131.5
+#lon_right=132
+#sensor='ls8'
+#year='2018'
 
 
 python3 ${pwd}"load_geomedian_data.py" $lat_top $lat_bottom $lon_left $lon_right $sensor $year $dir
